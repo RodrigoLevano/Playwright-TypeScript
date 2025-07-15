d51f7635d920ba2b8f1538927f4326f3a49657e8
Rodrigo Levano <rodrigo.alevano@gmail.com>
2025-07-15 17:21:01 -0500

    Finalización del curso de Playwright

--- tests/APITest/EjemploMock.spec.ts
+++ tests/APITest/EjemploMock.spec.ts
@@ -10,3 +10,17 @@ test('Hacer un mock de una fruta que no viene de la API real', async ({ page }) 
 
     await expect(page.getByText('Melocotón')).toBeVisible();
 })
+
+test('Obtengo la respuesta real y le agrego algo no tan real', async ({ page }) => {
+    await page.route('*/**/api/v1/fruits', async route => {
+        const response = await route.fetch();
+        const json = await response.json();
+        json.push({ name: 'Lionel Messi', id:200})
+
+        await route.fulfill({ response, json});
+    })
+
+    await page.goto('https://demo.playwright.dev/api-mocking');
+
+    await expect(page.getByText('Lionel Messi', {exact:true})).toBeVisible();
+})
